1c38b219c36315136305596f6799f6766371b11b
Maplebb <928085610@qq.com>
2021-11-30 17:54:59 +0800

    正在建设中-_-

--- app.py
+++ app.py
@@ -9,19 +9,29 @@ app = Flask(__name__)
 def music():
     mp3_dir = "./static/mp3"
     lyrics_dir = "./static/lyrics"
+    pics_dir = "./static/pic"
     mp3_files = os.listdir(mp3_dir)
-    print(mp3_files)
     lyrics_files = os.listdir(lyrics_dir)
+    pics_files = os.listdir(pics_dir)
     mp3_lyrics_arr = []
     for each_mp3 in mp3_files:
         mp3_name = each_mp3.split('.')[0]
         for each_lyrics in lyrics_files:
             lyrics_name = each_lyrics.split('.')[0]
             if (mp3_name == lyrics_name):
+                info = []
                 with open("./static/lyrics/"+each_lyrics, "r", encoding="utf-8") as f:
                     data = f.read()
-                mp3_lyrics_arr.append([mp3_name, data])
+                info.append(mp3_name)
+                info.append(data)
+                for each_pic in pics_files:
+                    pic_name = each_pic.split('.')[0]
+                    if (mp3_name == pic_name):
+                        info.append(each_pic)
+                        break
+                mp3_lyrics_arr.append(info)
                 break
+    print(mp3_lyrics_arr)
     return render_template("index.html", music_data=json.dumps(mp3_lyrics_arr))
 
 
--- static/js/lyrics.js
+++ static/js/lyrics.js
@@ -1,3 +1,4 @@
+let content = document.querySelector('.content')
 let lyricsDiv = document.querySelector('.lyrics');
 let music_data = document.querySelector('#music-data');
 let audio = document.createElement('audio');
@@ -5,21 +6,29 @@ let song_name = document.querySelector('.song-name');
 let singer = document.querySelector('.singer');
 let music_current_time = document.querySelector('.music-now-time');
 let music_total_time = document.querySelector('.music-total-time');
-let play_pause = document.querySelector('.play-and-pause');
 let icon_play_pause = document.querySelector('#icon-play-pause');
 let slider = document.querySelector('.music-played-time-bar');
 let buffer = document.querySelector('.music-loaded-time-bar');
 let music_bg = document.querySelector('.music-bg');
-let music_bg_mask = document.querySelector('.music-bg-mask');
+let music_next_mode = document.querySelector('#music-next-mode');
+let menu_bottom = document.querySelector('.menu-bottom');
+let description = document.querySelector('.description')
 
 
 let music_data_arr = JSON.parse(music_data.getAttribute('d'));
 music_data.remove();
 
 let now_song_num = 0;
+let total_songs = music_data_arr.length;
 let play_state = false;
 let current_lyric_index_pre;
 let current_lyric_index_now;
+let music_next_mode_num = 0;
+let music_next_mode_arr = ["icon in-order","icon loop","icon random"];
+// 0 for one by one,1 for single loop,2 for random
+let content_display_mode = 0
+// 0 for lyrics,1 for song list
+
 
 
 function split(){//把lrc歌词分割成数组，
@@ -62,15 +71,18 @@ function load_track() {
     slider.style.width = '0px';
 
     audio.src = "../static/mp3/"+music_data_arr[now_song_num][0]+".mp3";
-    song_name.innerHTML = music_data_arr[now_song_num][0].split(' ')[0];
-    singer.innerHTML = music_data_arr[now_song_num][0].split(' ')[1];
+    song_name.innerHTML = music_data_arr[now_song_num][0].split('-')[0];
+    singer.innerHTML = music_data_arr[now_song_num][0].split('-')[1];
+    music_bg.setAttribute('style',"background-image: url(\"../static/pic/"+music_data_arr[now_song_num][2]+"\");");
+    console.log(music_data_arr[now_song_num][2])
     audio.autoplay = true;
-    audio.loop = true;
+    audio.loop = false;
     // audio.load();
 
     music_total_time.innerHTML = "0:00";
     music_current_time.innerHTML = '0:00';
-    music_bg.setAttribute('style',"background-image: url(\"../static/pic/"+music_data_arr[now_song_num][0]+".jpg\");");
+    let scroll_height = String(lyricsDiv.parentNode.offsetHeight*0.5-current_lyric_index_now*32-16)+"px";
+    lyricsDiv.setAttribute('style',"transform: "+"translate3d(0px, "+scroll_height+", 0px);")
 }
 load_track();
 
@@ -98,7 +110,10 @@ function pausesong() {
 }
 
 function current(){
-    music_total_time.innerHTML = String(Math.floor(Number(audio.duration)/60)) + ":" + (Math.floor(Number(audio.duration)%60)<10?("0"+String(Math.floor(Number(audio.duration)%60))):String(Math.floor(Number(audio.duration)%60)));
+    if(!isNaN(Number(audio.duration)))
+        music_total_time.innerHTML = String(Math.floor(Number(audio.duration) / 60)) + ":" + (Math.floor(Number(audio.duration) % 60) < 10 ? ("0" + String(Math.floor(Number(audio.duration) % 60))) : String(Math.floor(Number(audio.duration) % 60)));
+    else
+        music_total_time.innerHTML = "0:00";
     music_current_time.innerHTML = String(Math.floor(Number(audio.currentTime)/60)) + ":" + (Math.floor(Number(audio.currentTime)%60)<10?("0"+String(Math.floor(Number(audio.currentTime)%60))):String(Math.floor(Number(audio.currentTime)%60)));
 
     slider.style.width = "calc("+String(audio.currentTime/audio.duration*100)+"%"+" - "+String(audio.currentTime/audio.duration*10)+"px)";
@@ -111,7 +126,6 @@ function current(){
         current_lyric_index_pre = current_lyric_index_now
     }
     let scroll_height = String(lyricsDiv.parentNode.offsetHeight*0.5-current_lyric_index_now*32-16)+"px";
-    console.log("translate3d(0px, "+scroll_height+", 0px);");
     lyricsDiv.setAttribute('style',"transform: "+"translate3d(0px, "+scroll_height+", 0px);")
 }
 
@@ -127,8 +141,59 @@ function current_lyric(){
     }
     return i-1
 }
+function init() {
+    lyricsDiv.innerHTML = "";
+    lyricsArr = split();
+    createPara();
+    load_track();
+    icon_play_pause.setAttribute('class', "icon icon-play")
+}
+// bug
+function presong(){
+    now_song_num = (now_song_num==0)?(total_songs-1):(now_song_num-1)%total_songs;
+    init();
+}
+function nextsong_onorder(){
+    now_song_num = (now_song_num+1)%total_songs;
+    init();
+}
+// get random int(from 0 to max-1)
+function getrandomint(max) {
+    return Math.floor(Math.random()*max)
+}
+function nextsong_random(){
+    now_song_num = getrandomint(total_songs)
+    init()
+}
+function nextsong(){
+    switch (music_next_mode_num) {
+        case 0:{
+            nextsong_onorder();
+            break;
+        }
+        case 1:{
+            init();
+            break;
+        }
+        case 2:{
+            nextsong_random();
+            break;
+        }
+    }
+}
 audio.ontimeupdate = current;
 audio.onplay = function(){
     play_state = true;
     icon_play_pause.setAttribute('class', "icon icon-pause");
+};
+audio.onended = nextsong;
+
+function click_next_mode(){
+    music_next_mode_num = (music_next_mode_num+1)%3;
+    music_next_mode.setAttribute("class",music_next_mode_arr[music_next_mode_num]);
+}
+
+function change_display(){
+    content.style.display = "none";
+    description.style.display = "flex";
 }
